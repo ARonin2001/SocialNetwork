0a7c8208d3a5cb81116dae0684c7aa4a411379c6
Alex <Aronin.2001@gmail.com>
2022-04-27 16:15:41 +0400

    Messages page finished

--- src/js/messages.js
+++ src/js/messages.js
@@ -2,6 +2,28 @@ const dialogsUser = document.querySelectorAll('.d-user');
 
 dialogsUser.forEach((el) => {
     el.addEventListener('click', e => {
+        for(let i = 0; i < dialogsUser.length; i++) {
+            deleteActiveClass(dialogsUser[i]);
+        }
         el.classList.toggle('active');
     })
+})
+
+const deleteActiveClass = (e) => {
+    if(e.classList.contains('active')) {
+        e.classList.remove('active');
+    }
+}
+
+const textareaPlaceholder = document.querySelector('.s-message__textarea-placeholder'),
+    writer = document.querySelector('#s-message__writer');
+
+textareaPlaceholder.addEventListener('click', () => {
+    textareaPlaceholder.classList.remove('active');
+    writer.classList.add('active')
+    writer.focus();
+})
+writer.addEventListener('blur', () => {
+    writer.classList.remove('active');
+    textareaPlaceholder.classList.add('active')
 })
